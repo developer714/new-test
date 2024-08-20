624ef33fa98f847e12ecfd8bc62788d28bebd75b
unknown <dev.private102@gmail.com>
2024-08-20 14:46:21 -0500

    New V2

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -4,6 +4,6 @@ const resolvers = require("./resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen({ path: "/api" }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });

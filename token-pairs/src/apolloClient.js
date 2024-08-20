71fc230c0bc83d526c3eb6aae76094f25b454d71
unknown <dev.private102@gmail.com>
2024-08-20 14:16:11 -0500

    New

--- token-pairs/src/apolloClient.js
+++ token-pairs/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "/api/graphql", // Replace with the actual GraphQL server URL
+    uri: "http://localhost:4000", // Replace with the actual GraphQL server URL
   }),
   cache: new InMemoryCache(),
 });

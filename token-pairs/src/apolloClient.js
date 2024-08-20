a0107320f940f54611d409b8b1a882f9f0648a19
unknown <dev.private102@gmail.com>
2024-08-20 13:52:29 -0500

    Updated like HelloWorld

--- token-pairs/src/apolloClient.js
+++ token-pairs/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "/api", // Replace with the actual GraphQL server URL
+    uri: "/api/graphql", // Replace with the actual GraphQL server URL
   }),
   cache: new InMemoryCache(),
 });
